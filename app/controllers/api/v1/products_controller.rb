3fff182ba2ecb69706762d1d13c0bac05c800f6b
Jose Alan <alan123art@gmail.com>
2019-02-23 16:50:46 -0300

    restcrud basic

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,19 +1,70 @@
 class Api::V1::ProductsController < ApplicationController
+	before_action :find_product, only: [:find, :update, :destroy]
+
 	def hello_world
 		render json: {message: 'Hello API'}
 	end
 	def list
-		render json: Product.all
+		products = Product.all
+		render json: products, status: :ok 
 	end
+
+	#create
 	def create
 		product = Product.new
 		product.name = params[:name]
 		product.quantity = params[:quantity]
 
 		if product.save
-			render json: product
-		else
-			render json: {"error": "product could'n be saved"}
+			render json: product, status: :created and return
+		elsif product.has_nil_fields?
+			error_status = :bad_request
+		else	
+			error_status = :unprocessable_entity
 		end	
+
+		render json: {message: 'Product not saved', errors: product.errors}, status: error_status
 	end
+
+	#delete
+	def destroy
+		@product = Product.find_by_id(params[:id])
+		if @product.nil?
+			render json: {message: "Product not found"}, status: :not_found
+		else	
+			@product.destroy
+		end
+	end
+
+	def update
+		#product = Product.find_by_id(params[:id])
+		@product.name = params[:name]
+		@product.quantity = params[:quantity]
+
+		if product.save
+			render json: @product, status: :ok and return
+		elsif product.has_nil_fields?
+			error_status = :bad_request
+		else	
+			error_status = :unprocessable_entity
+		end	
+
+		render json: {message: 'Product not update', errors: @product.errors}, status: error_status
+
+	end
+
+	def find
+		
+		if @product.nil?
+			render json: {message: "Product not found"}, status: :not_found
+		else
+			render json: @product, status: :ok 
+		end
+
+	end
+
+	private
+	def find_product
+		@product = Product.find_by_id(params[:id])
+	end	
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ApplicationRecord
+	validates :name, presence: true
+	validates :quantity, presence: true
+
+	def has_nil_fields?
+		self.name.nil? || self.quantity.nil?
+	end
 end
